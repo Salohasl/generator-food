3ee1317628f4b02bd20182e69538ca7dda818823
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-04-21 18:53:45 +0200

    first commit

---
+++ src/foodArray.ts
@@ -0,0 +1,54 @@
+export default [
+    {
+      "id":"1",
+      "name":"Вок",
+      "src":"/dist/img/wok.jpg"
+    },
+    {
+      "id":"2",
+      "name":"Плов",
+      "src":"/dist/img/plov.jpg"
+    },
+    {
+      "id":"3",
+      "name":"Гречка",
+      "src":"/dist/img/gr.jpg"
+    },
+    {
+      "id":"4",
+      "name":"Макароны",
+      "src":"/dist/img/mak.jpg"
+    },
+    {
+      "id":"5",
+      "name":"Жаркое",
+      "src":"/dist/img/jar.jpg"
+    },
+    {
+      "id":"6",
+      "name":"Жаренная картошка",
+      "src":"/dist/img/cart.jpg"
+    },
+    {
+      "id":"7",
+      "name":"Пюре",
+      "src":"/dist/img/pure.jpg"
+    },
+    {
+      "id":"8",
+      "name":"Креативчик(что-то новое)",
+      "src":"/dist/img/hz.jpg"
+    },
+    {
+      "id":"9",
+      "name":"Пицца",
+      "src":"/dist/img/pizza.jpg"
+    },
+    {
+      "id":"10",
+      "name":"Шаурма",
+      "src":"/dist/img/shaur.jpg"
+    }    
+];
+
+
